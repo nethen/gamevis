7a1039bb4459e37525d2abead5d3fab9966d2c42
nethen <72537484+nethen@users.noreply.github.com>
2025-04-12 18:15:08 -0700

    feat: list all valid annotations

--- gamevis_explorer/src/app/page.tsx
+++ gamevis_explorer/src/app/page.tsx
@@ -1,13 +1,45 @@
 import Image from "next/image";
 import meta from "@/app/meta.json";
+import data from "@/app/data.json";
 
-export default function Home() {
+const uniqueGenres = [...new Set(meta.map((item) => item.genre))];
+const sortedData = data.sort((a, b) => {
+  return Number(a.game_id) - Number(b.game_id);
+});
+
+export default function Page() {
   // console.log(meta);
   return (
-    <ul>
-      {meta.map((item) => (
-        <li key={item.id}>{item.name}</li>
-      ))}
-    </ul>
+    <div className="flex">
+      <nav>
+        <div className="mb-8">
+          <h2 className="text-lg">Games</h2>
+          <ul>
+            {meta.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="mb-8">
+          <h2 className="text-lg">Genres</h2>
+          <ul>
+            {uniqueGenres ? (
+              uniqueGenres.map((item) => <li key={item}>{item}</li>)
+            ) : (
+              <p>Loading genres</p>
+            )}
+          </ul>
+        </div>
+      </nav>
+      <main className="flex flex-col items-center justify-center min-h-screen p-24 text-center bg-red-500 w-full">
+        <ul>
+          {sortedData.map((item, i) => (
+            <li key={i}>
+              {item.game_id + "_" + item.screenshot_id + "_" + item.vis_id}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
